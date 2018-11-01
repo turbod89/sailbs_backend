4ec9b62e7ebe80c80a98407fcc458fb233164ca1
Daniel Torres <dani10sa2@hotmail.com>
2018-11-01 22:59:59 +0100

    Modified test controller and routes to gitignore

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,6 +3,23 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
+
+/*
+ |------------------------------------------------
+ | Test
+ |------------------------------------------------
+ |
+ | Git will not keep track of this file from now on.
+ |
+ | Feel free to modify it all as you want to make
+ | your tests.
+ |
+ | Notice that Test Routes will also remain untracked.
+ |
+ | More info: https://stackoverflow.com/questions/3319479/can-i-git-commit-a-file-and-ignore-its-content-changes
+ |
+ */
+
 class TestController extends BaseController
 {
     /**
