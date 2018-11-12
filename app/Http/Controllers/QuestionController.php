2b88efc2a8fc3f8391db5a99f6d9a3ce3d71de6f
Daniel Torres <dani10sa2@hotmail.com>
2018-11-13 00:17:43 +0100

    Created Question controller, routes.

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class QuestionController extends BaseController
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    //
+}
