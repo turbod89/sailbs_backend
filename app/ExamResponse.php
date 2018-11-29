371ec233e95f2cab0140f41384dfe1de877f7b11
Daniel Torres <dani10sa2@hotmail.com>
2018-11-29 22:51:52 +0100

    Format fix.

--- app/ExamResponse.php
+++ app/ExamResponse.php
@@ -72,9 +72,9 @@ class ExamResponse extends BaseModel {
             select
                 asked.subject_id as subject_id,
                 asked.num_questions as num_questions,
-                ifnull(answered.num_answered,0) as num_answered,
-                ifnull(answered.num_correct,0) as num_correct,
-                asked.num_questions - ifnull(answered.num_correct,0) as num_errors,
+                convert(ifnull(answered.num_answered,0), unsigned integer) as num_answered,
+                convert(ifnull(answered.num_correct,0), unsigned integer) as num_correct,
+                convert(asked.num_questions - ifnull(answered.num_correct,0), unsigned integer) as num_errors,
                 asked.max_errors as max_errors,
                 case
                     when max_errors is null then 1
@@ -124,15 +124,15 @@ class ExamResponse extends BaseModel {
 
         $query = "
             select
-                asqued.exam_response_id as exam_response_id,
-                asqued.num_questions as num_questions,
-                ifnull(answered.num_answered,0) as num_answered,
-                ifnull(answered.num_correct,0) as num_correct,
-                asqued.num_questions - ifnull(answered.num_correct,0) as num_errors,
-                asqued.max_errors as max_errors,
+                asked.exam_response_id as exam_response_id,
+                asked.num_questions as num_questions,
+                convert(ifnull(answered.num_answered,0), unsigned integer) as num_answered,
+                convert(ifnull(answered.num_correct,0) , unsigned integer) as num_correct,
+                convert(asked.num_questions - ifnull(answered.num_correct,0), unsigned integer) as num_errors,
+                asked.max_errors as max_errors,
                 case
                     when max_errors is null then 1
-                    when max_errors >= asqued.num_questions - ifnull(answered.num_correct,0) then 1
+                    when max_errors >= asked.num_questions - ifnull(answered.num_correct,0) then 1
                     else 0
                 end as exam_passed
                 
@@ -148,7 +148,7 @@ class ExamResponse extends BaseModel {
                     inner join questions q on q.id = qe.question_id
                     group by
                         er.id
-                ) as asqued
+                ) as asked
             
             left join
                 (
@@ -162,7 +162,7 @@ class ExamResponse extends BaseModel {
                     group by
                         er.id
                 ) as answered
-            on asqued.exam_response_id = answered.exam_response_id
+            on asked.exam_response_id = answered.exam_response_id
         ";
 
         $summary = DB::select($query,[$this->id,$this->id]);
@@ -194,6 +194,7 @@ class ExamResponse extends BaseModel {
         $json['user_id'] = $this->user->id;
         $json['certificate_code'] = $this->exam->certificate->code;
         $json['summary'] = $this->summary;
+        $json['subject_summary'] = $this->subject_summary;
 
         return $json;
     }
